b2bd42456752497d4e7ce30247c04e7f73027452
JINZHANG2017 <zj574406254@hotmail.com>
2020-10-17 15:10:16 +0800

    [ZhangJin] feat: finish exercises

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,14 +1,28 @@
 import { register } from "../user";
+import { verifyPassword, verifyUsername } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
+  test("should post user when validated", async() => {
     // TODO 19: add test here
+    verifyPassword.mockReturnValueOnce(true);
+    verifyUsername.mockReturnValueOnce(true);
+
+    const result=register("username","pwd");
+
+    await expect(result).resolves.toEqual({});
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async() => {
     // TODO 20: add test here
+    verifyPassword.mockReturnValueOnce(true);
+    verifyUsername.mockReturnValueOnce(false);
+    // const result=register("username","pwd");
+    await expect(() => 
+    {register("username","pwd");}
+       ).toThrow("wrong username or password");
+    
   });
 });
--- src/matchers/__test__/expect.calls.test.js
+++ src/matchers/__test__/expect.calls.test.js
@@ -6,7 +6,10 @@ test("TODO 11", () => {
   const mockCallback = jest.fn((x) => 42 + x);
   forEach([1, 2], mockCallback);
 
-  // TODO 11: add assertion
+  expect(mockCallback).toBeCalledTimes(2);
+  expect(mockCallback).toBeCalledWith(1);
+  expect(mockCallback).toBeCalledWith(2);
+
 });
 
 test("TODO 12", () => {
@@ -16,9 +19,8 @@ test("TODO 12", () => {
     .mockReturnValueOnce("string")
     .mockReturnValue(true);
 
-  // TODO 12: to add "expected" value
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
+  expect(mockFn()).toBe(42);
+  expect(mockFn()).toBe("string");
+  expect(mockFn()).toBe(true);
+  expect(mockFn()).toBe(true);
 });
